60f11c769cad0493f31baa9da965fe2e5d72f450
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-10-20 17:57:21 +0200

    Add a function to build a hash from a string.

--- src/Hash.lua
+++ src/Hash.lua
@@ -1,6 +1,19 @@
 local hash = {}
 
-hash.tMhash = require("mhash")
+
+-- Get the mhash plugin for the hash algorithms.
+hash.mhash = require("mhash")
+
+
+
+function hash:bin_to_hex(strBin)
+  local aHashHex = {}
+  for iCnt=1,string.len(strBin) do
+    table.insert(aHashHex, string.format("%02x", string.byte(strBin, iCnt)))
+  end
+  local strHashHex = table.concat(aHashHex)
+end
+
 
 
 function hash:check_sha1(strPath, strSha1)
@@ -13,7 +26,7 @@ function hash:check_sha1(strPath, strSha1)
   tState:init(self.tMhash.MHASH_SHA1)
 
   -- Open the file and read it in chunks.
-  tFile = io.open(strPath, "rb")
+  local tFile = io.open(strPath, "rb")
   if tFile==nil then
     fOk = false
     tResult = string.format("Failed to open the file for reading: %s", strPath)
@@ -24,16 +37,12 @@ function hash:check_sha1(strPath, strSha1)
         tState:hash(tChunk)
       end
     until tChunk==nil
-    strHashBin = tState:hash_end()
-
     tFile:close()
 
+    local strHashBin = tState:hash_end()
+
     -- Convert the binary hash into a string.
-    local aHashHex = {}
-    for iCnt=1,string.len(strHashBin) do
-      table.insert(aHashHex, string.format("%02x", string.byte(strHashBin, iCnt)))
-    end
-    local strHashHex = table.concat(aHashHex)
+    local strHashHex = self:bin_to_hex(strHashBin)
 
     local strHashExpected = string.lower(strSha1)
     if strHashExpected==strHashHex then
@@ -44,6 +53,41 @@ function hash:check_sha1(strPath, strSha1)
       fOk = false
       tResult = string.format("The hash does not match!\nExpected: %s\nRead:    %s", strHashExpected, strHashHex)
     end
+  end
+
+  return fOk,tResult
+end
+
+
+
+function hash:get_sha1_string(strData, strSha1)
+  -- Create a new MHash state for SHA1.
+  local tState = self.tMhash.mhash_state()
+  tState:init(self.tMhash.MHASH_SHA1)
+  tState:hash(strData)
+  local strHashBin = tState:hash_end()
+
+  -- Convert the binary hash into a string.
+  return self:bin_to_hex(strHashBin)
+end
+
+
+
+function hash:check_sha1_string(strData, strSha1)
+  local fOk = true
+  local tResult = nil
+
+
+  -- Convert the binary hash into a string.
+  local strHash = self:get_sha1_string(strData)
+
+  local strHashExpected = string.lower(strSha1)
+  if strHashExpected==strHashHex then
+    fOk = true
+    tResult = nil
+  else
+    fOk = false
+    tResult = string.format("The hash does not match!\nExpected: %s\nRead:    %s", strHashExpected, strHashHex)
   end
 
   return fOk,tResult
