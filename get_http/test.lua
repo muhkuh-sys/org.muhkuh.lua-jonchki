320bfa370f599cb135f81650a984b10dbf6ffd5f
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-12-08 16:34:31 +0100

    Use the old lua-curl binding in the test.

--- get_http/test.lua
+++ get_http/test.lua
@@ -1,33 +1,31 @@
 local curl = require 'curl'
-for k,v in pairs(curl) do
-	print (k,v)
-end
 
-local strURL = 'http://127.0.0.1/jonchki/org/muhkuh/lua/lua-romloader/2.2.2/lua51-romloader-2.2.2.xml'
+local strURL = 'https://github.com/muhkuh-sys/org.muhkuh.tools-flasher/releases/download/v1.3.0/ivy.xml'
 
+-- Collect the received data in a table.
+local atChunks = {}
+local sizTotal = 0
 
-local function curl_download(atChunks, aucBuffer)
+local function curl_download(aucBuffer)
 	table.insert(atChunks, aucBuffer)
-	return 0
-end
-
-local function curl_progress(tDummy, ulDlTotal, ulDlNow, ulUpTotal, ulUpNow)
-	print(string.format('%d%% (%d/%d)', ulDlTotal/ulDlNow*100, ulDlNow, ulDlTotal))
+	local sizNew = string.len(aucBuffer)
+  sizTotal = sizTotal + sizNew
+  print(string.format('Dl %d.', sizTotal)) 
+	return sizNew
 end
 
 tCURL = curl.easy_init()
 
-tCURL:setopt(tCURL.OPT_URL, strURL)
+tCURL:setopt(curl.OPT_FOLLOWLOCATION, 1)
+tCURL:setopt(curl.OPT_URL, strURL)
 
--- Collect the received data in a table.
-local atChunks = {}
-tCURL:setopt(tCURL.OPT_WRITEDATA, atChunks)
-tCURL:setopt(tCURL.OPT_WRITEFUNCTION, curl_download)
-tCURL:setopt(tCURL.OPT_PROGRESSDATA, nil)
-tCURL:setopt(tCURL.OPT_PROGRESSFUNCTION, curl_progress)
+--tCURL:setopt(curl.OPT_WRITEDATA, atChunks)
+tCURL:setopt(curl.OPT_WRITEFUNCTION, curl_download)
 
 tCURL:perform()
-tCURL:close()
+if tCURL.close~=nil then
+  tCURL:close()
+end
 
 local strData = table.concat(atChunks)
 print(strData)
