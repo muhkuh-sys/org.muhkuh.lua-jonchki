1f423f63b617cda9dce64ae102f6352d1690afc9
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-08-24 17:29:13 +0200

    Add the GIT version to the package.

--- build_artifacts.sh
+++ build_artifacts.sh
@@ -1,4 +1,51 @@
 #! /bin/bash
+
+# ----------------------------------------------------------------------------
+#
+# Try to get the VCS ID.
+#
+PROJECT_VERSION_VCS="unknown"
+PROJECT_VERSION_VCS_LONG="unknown"
+GIT_EXECUTABLE=$(which git)
+STATUS=$?
+if [ ${STATUS} -ne 0 ]
+then
+  echo "Git not found! Set the version to 'unknown'."
+else
+  GITV0=$(${GIT_EXECUTABLE} describe --abbrev=12 --always --dirty=+ --long)
+  if [ ${STATUS} -ne 0 ]
+  then
+    echo "Failed to run Git! Set the version to 'unknown'."
+  else
+    if [[ ${GITV0} =~ ^[0-9a-f]+\+?$ ]]
+    then
+      echo 'This is a repository with no tags. Use the raw SHA sum.'
+      PROJECT_VERSION_VCS="GIT${GITV0}"
+      PROJECT_VERSION_VCS_LONG="GIT${GITV0}"
+    elif [[ ${GITV0} =~ ^v([0-9.]+)-([0-9]+)-g([0-9a-f]+\+?)$ ]]
+    then
+      VCS_REVS_SINCE_TAG=${BASH_REMATCH[2]}
+      if [ ${VCS_REVS_SINCE_TAG} -eq 0 ]
+      then
+        echo 'This is a repository which is exactly on a tag. Use the tag name.'
+        PROJECT_VERSION_VCS="GIT${BASH_REMATCH[1]}"
+        PROJECT_VERSION_VCS_LONG="GIT${BASH_REMATCH[1]}-${BASH_REMATCH[3]}"
+      else
+        echo 'This is a repository with commits after the last tag. Use the checkin ID.'
+        PROJECT_VERSION_VCS="GIT${BASH_REMATCH[3]}"
+        PROJECT_VERSION_VCS_LONG="GIT${BASH_REMATCH[3]}"
+      fi
+    else
+      echo 'The description has an unknown format. Use the tag name.'
+      PROJECT_VERSION_VCS="GIT${GITV0}"
+      PROJECT_VERSION_VCS_LONG="GIT${GITV0}"
+    fi
+  fi
+fi
+echo "PROJECT_VERSION_VCS: ${PROJECT_VERSION_VCS}"
+echo "PROJECT_VERSION_VCS_LONG: ${PROJECT_VERSION_VCS_LONG}"
+
+# Errors are fatal from now on.
 set -e
 
 # Set the verbose level.
@@ -13,6 +60,10 @@ JONCHKI=${JONCHKI_PATH}/jonchki.lua
 
 # This is the base path where all packages will be assembled.
 WORK_BASE=${PRJ_DIR}/targets/jonchki
+
+# Write the GIT version into the template.
+rm -f ${WORK_BASE}/jonchki.xml
+sed --expression="s/\${PROJECT_VERSION_VCS_LONG}/${PROJECT_VERSION_VCS_LONG}/" installer/jonchki_template.xml >${WORK_BASE}/jonchki.xml
 
 # Remove all working folders and re-create them.
 rm -rf ${WORK_BASE}/windows_32bit
@@ -34,7 +85,7 @@ mkdir -p ${WORK_BASE}/ubuntu_17.04_32bit
 mkdir -p ${WORK_BASE}/ubuntu_17.04_64bit
 
 # The common options are the same for all targets.
-COMMON_OPTIONS="--syscfg ${PRJ_DIR}/installer/jonchkisys.cfg --prjcfg ${PRJ_DIR}/installer/jonchkicfg.xml --finalizer ${PRJ_DIR}/installer/finalizer.lua ${PRJ_DIR}/installer/jonchki.xml"
+COMMON_OPTIONS="--syscfg ${PRJ_DIR}/installer/jonchkisys.cfg --prjcfg ${PRJ_DIR}/installer/jonchkicfg.xml --finalizer ${PRJ_DIR}/installer/finalizer.lua ${WORK_BASE}/jonchki.xml"
 
 # Build the Windows_x86 artifact.
 pushd ${WORK_BASE}/windows_32bit
