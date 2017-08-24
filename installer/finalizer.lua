ad6afa4b8134599e9bb9d714d80e6cc223902a27
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-08-24 14:04:39 +0200

    Add a package file with information about the artifact.

--- installer/finalizer.lua
+++ installer/finalizer.lua
@@ -2,6 +2,9 @@ local t = ...
 local strDistId, strDistVersion, strCpuArch = t:get_platform()
 local cLogger = t.cLogger
 local tResult
+local archives = require 'installer.archives'
+local pl = require'pl.import_into'()
+
 
 -- Copy all jonchki scripts.
 local atScripts = {
@@ -36,39 +39,51 @@ for strSrc, strDst in pairs(atScripts) do
 end
 
 
-local archives = require 'installer.archives'
-local Archive = archives(cLogger)
-local pl = require'pl.import_into'()
-
--- Create a ZIP archive for Windows platforms. Build a "tar.gz" for Linux.
-local strArchiveExtension
-local tFormat
-local atFilter
-if strDistId=='windows' then
-  strArchiveExtension = 'zip'
-  tFormat = Archive.archive.ARCHIVE_FORMAT_ZIP
-  atFilter = {}
+-- Create the package file.
+local strPackageText = t:replace_template([[PACKAGE_NAME=${root_artifact_artifact}
+PACKAGE_VERSION=${root_artifact_version}
+PACKAGE_VCS_ID=${root_artifact_vcs_id}
+HOST_DISTRIBUTION_ID=${platform_distribution_id}
+HOST_DISTRIBUTION_VERSION=${platform_distribution_version}
+HOST_CPU_ARCHITECTURE=${platform_cpu_architecture}
+]])
+local strPackagePath = t:replace_template('${install_base}/.jonchki/package.txt')
+local tFileError, strError = pl.utils.writefile(strPackagePath, strPackageText, false)
+if tFileError==nil then
+  cLogger:error('Failed to write the package file "%s": %s', strPackagePath, strError)
 else
-  strArchiveExtension = 'tar.gz'
-  tFormat = Archive.archive.ARCHIVE_FORMAT_TAR_GNUTAR
-  atFilter = { Archive.archive.ARCHIVE_FILTER_GZIP }
-end
+  local Archive = archives(cLogger)
 
--- Translate the CPU architecture to bits.
-local atCpuArchToBits = {
-  ['x86'] = 32,
-  ['x86_64'] = 64
-}
-local uiPlatformBits = atCpuArchToBits[strCpuArch]
-if uiPlatformBits==nil then
-  cLogger:error('Failed to translate the CPU architecture "%s" to bits.', strCpuArch)
-else
-  local strArtifactVersion = t:replace_template('${root_artifact_artifact}-${root_artifact_version}')
-  local strArchive = t:replace_template(string.format('${install_base}/../%s_%s%s_%dbit.%s', strArtifactVersion, strDistId, strDistVersion, uiPlatformBits, strArchiveExtension))
-  local strDiskPath = t:replace_template('${install_base}')
-  local strArchiveMemberPrefix = strArtifactVersion
+  -- Create a ZIP archive for Windows platforms. Build a "tar.gz" for Linux.
+  local strArchiveExtension
+  local tFormat
+  local atFilter
+  if strDistId=='windows' then
+    strArchiveExtension = 'zip'
+    tFormat = Archive.archive.ARCHIVE_FORMAT_ZIP
+    atFilter = {}
+  else
+    strArchiveExtension = 'tar.gz'
+    tFormat = Archive.archive.ARCHIVE_FORMAT_TAR_GNUTAR
+    atFilter = { Archive.archive.ARCHIVE_FILTER_GZIP }
+  end
 
-  tResult = Archive:pack_archive(strArchive, tFormat, atFilter, strDiskPath, strArchiveMemberPrefix)
+  -- Translate the CPU architecture to bits.
+  local atCpuArchToBits = {
+    ['x86'] = 32,
+    ['x86_64'] = 64
+  }
+  local uiPlatformBits = atCpuArchToBits[strCpuArch]
+  if uiPlatformBits==nil then
+    cLogger:error('Failed to translate the CPU architecture "%s" to bits.', strCpuArch)
+  else
+    local strArtifactVersion = t:replace_template('${root_artifact_artifact}-${root_artifact_version}')
+    local strArchive = t:replace_template(string.format('${install_base}/../%s_%s%s_%dbit.%s', strArtifactVersion, strDistId, strDistVersion, uiPlatformBits, strArchiveExtension))
+    local strDiskPath = t:replace_template('${install_base}')
+    local strArchiveMemberPrefix = strArtifactVersion
+
+    tResult = Archive:pack_archive(strArchive, tFormat, atFilter, strDiskPath, strArchiveMemberPrefix)
+  end
 end
 
 return tResult
