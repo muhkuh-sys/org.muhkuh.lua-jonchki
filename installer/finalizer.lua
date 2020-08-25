73083620b746c52b831c2f8a65cf285d588125c0
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2020-08-26 00:04:14 +0200

    Use the new installer methods for the package and hash files and to create the artifact.

--- installer/finalizer.lua
+++ installer/finalizer.lua
@@ -1,14 +1,9 @@
 local t = ...
 local strDistId, strDistVersion, strCpuArch = t:get_platform()
-local cLog = t.cLog
-local tLog = t.tLog
-local tResult
-local archives = require 'installer.archives'
-local pl = require'pl.import_into'()
 
 
 -- Copy all jonchki scripts.
-local atScripts = {
+t:install{
   ['../jonchki.lua']                                  = '${install_base}/',
   ['../doc/jonchkicfg.xml']                           = '${install_doc}/',
   ['../doc/jonchkireport.xsl']                        = '${install_doc}/',
@@ -36,9 +31,6 @@ local atScripts = {
   ['../lua/SystemConfiguration.lua']                  = '${install_lua_path}/',
   ['${report_path}']                                  = '${install_base}/.jonchki/'
 }
-for strSrc, strDst in pairs(atScripts) do
-  t:install(strSrc, strDst)
-end
 
 -- Install the wrapper.
 if strDistId=='windows' then
@@ -49,40 +41,13 @@ elseif strDistId=='ubuntu' then
 end
 
 -- Create the package file.
-local strPackageText = t:replace_template([[PACKAGE_NAME=${root_artifact_artifact}
-PACKAGE_VERSION=${root_artifact_version}
-PACKAGE_VCS_ID=${root_artifact_vcs_id}
-HOST_DISTRIBUTION_ID=${platform_distribution_id}
-HOST_DISTRIBUTION_VERSION=${platform_distribution_version}
-HOST_CPU_ARCHITECTURE=${platform_cpu_architecture}
-]])
-local strPackagePath = t:replace_template('${install_base}/.jonchki/package.txt')
-local tFileError, strError = pl.utils.writefile(strPackagePath, strPackageText, false)
-if tFileError==nil then
-  tLog.error('Failed to write the package file "%s": %s', strPackagePath, strError)
-else
-  local Archive = archives(cLog)
+t:createPackageFile()
 
-  -- Create a ZIP archive for Windows platforms. Build a "tar.gz" for Linux.
-  local strArchiveExtension
-  local tFormat
-  local atFilter
-  if strDistId=='windows' then
-    strArchiveExtension = 'zip'
-    tFormat = Archive.archive.ARCHIVE_FORMAT_ZIP
-    atFilter = {}
-  else
-    strArchiveExtension = 'tar.gz'
-    tFormat = Archive.archive.ARCHIVE_FORMAT_TAR_GNUTAR
-    atFilter = { Archive.archive.ARCHIVE_FILTER_GZIP }
-  end
+-- Create a hash file.
+t:createHashFile()
 
-  local strArtifactVersion = t:replace_template('${root_artifact_artifact}-${root_artifact_version}')
-  local strArchive = t:replace_template(string.format('${install_base}/../../../../%s-%s%s_%s.%s', strArtifactVersion, strDistId, strDistVersion, strCpuArch, strArchiveExtension))
-  local strDiskPath = t:replace_template('${install_base}')
-  local strArchiveMemberPrefix = strArtifactVersion
+-- Build the artifact.
+t:createArchive('${install_base}/../../../..', 'native')
 
-  tResult = Archive:pack_archive(strArchive, tFormat, atFilter, strDiskPath, strArchiveMemberPrefix)
-end
+return true
 
-return tResult
