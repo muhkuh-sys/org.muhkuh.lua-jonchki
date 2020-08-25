c5adafc6d3606c8d852bf2673aece40eeb96b545
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2020-08-25 21:47:04 +0200

    Accept a table with a src->dst mapping.

--- lua/installer/install_helper.lua
+++ lua/installer/install_helper.lua
@@ -177,154 +177,161 @@ function InstallHelper:install(tSrc, strDst)
     error('The "install" method was called without a proper "self" argument. Use "t:install(source, destination)" to call the function.')
   end
 
-  -- The second argument must be either a list of strings or a string.
-  local astrSrc = nil
   local strSrcType = type(tSrc)
-  if strSrcType=='string' then
-    astrSrc = { tSrc }
-  elseif strSrcType=='table' then
-    astrSrc = tSrc
-  else
-    error(string.format('The "install" method was called with an invalid "tSrc" argument. It must be either a string or a table.'))
-  end
-  -- Loop over all elements and check their type.
-  local astrErrors = {}
-  for uiCnt, tValue in ipairs(astrSrc) do
-    local strType = type(tValue)
-    if strType~='string' then
-      table.insert(astrErrors, tostring(uiCnt))
-    end
-  end
-  if #astrErrors~=0 then
-    error(string.format('The "install" method was called with an invalid "tSrc" argument. The table contains non-string elements at the following indices: %s', table.concat(astrErrors, ', ')))
-  end
-
-  -- The third argument must be a string.
-  if type(strDst)~='string' then
-    error(string.format('The "install" method was called with an invalid "strDst" argument. It must be a string.'))
-  end
-
-  -- Replace the ${} strings.
-  for uiCnt, strValue in ipairs(astrSrc) do
-    astrSrc[uiCnt] = self:replace_template(strValue)
-  end
-  local strDst = self:replace_template(strDst)
-
-  -- The destination is treated as a directory...
-  --   if it ends with a slash or
-  --   if the source is a list with more than one element.
-  local fDstIsDir = nil
-  if string.sub(strDst, -1)=='/' then
-    fDstIsDir = true
-  elseif #astrSrc>1 then
-    fDstIsDir = true
-  else
-    fDstIsDir = false
-  end
-
-  -- Get the directory part of the destination.
-  local strDstDirname = nil
-  local strDstFilename = nil
-  if fDstIsDir==true then
-    strDstDirname = strDst
-  else
-    strDstDirname, strDstFilename = self.pl.path.splitpath(strDst)
-  end
-
-  -- Loop over all elements in the source list.
-  for _, strSrc in pairs(astrSrc) do
-    self.tLogInstallHelper.info('Installing "%s"...', strSrc)
-    -- Get the absolute path for the current source.
-    local strSrcAbs = self.pl.path.abspath(strSrc, self.strCwd)
-
-    -- Does the source exist?
-    if self.pl.path.exists(strSrcAbs)~=strSrcAbs then
-      error(string.format('The source path "%s" does not exist.', strSrcAbs))
+  if strSrcType=='table' and strDst==nil then
+    for tSrcCnt, tDstCnt in pairs(tSrc) do
+      self:install(tSrcCnt, tDstCnt)
     end
 
-    -- Is the source a folder or a file.
-    local fIsDir = self.pl.path.isdir(strSrcAbs)
-    local fIsFile = self.pl.path.isfile(strSrcAbs)
-    if (fIsDir==true) and (fIsFile==true) then
-      error(string.format('"%s" is both a file and a directory.', strSrcAbs))
-    elseif (fIsDir==false) and (fIsFile==false) then
-      error(string.format('"%s" is neither a file nor a directory.', strSrcAbs))
-    end
-
-    if fIsFile==true then
-      -- Copy one single file.
-
-      -- Get the filename of the source without the directory part.
-      local strSrcFilename = self.pl.path.basename(strSrcAbs)
-
-      -- Get the destination path.
-      local strDstPath = nil
-      if fDstIsDir==true then
-        strDstPath = self.pl.path.join(strDstDirname, strSrcFilename)
-      else
-        strDstPath = self.pl.path.join(strDstDirname, strDstFilename)
-      end
-
-      -- FIXME: check if the path is below the install base folder.
-
-      -- Was this file already installed?
-      local strPackage = self.atInstalledFiles[strDstPath]
-      if strPackage~=nil then
-        -- Yes -> refuse to overwrite it.
-        error(string.format('The file "%s" was already installed by the artifact %s.', strDstPath, strPackage))
-      end
-      self.atInstalledFiles[strDstPath] = self.strGMAV
-
-      -- Create the output folder.
-      local tResult, strError = self.pl.dir.makepath(strDstDirname)
-      if tResult~=true then
-        error(string.format('Failed to create the output folder "%s": %s', strDstDirname, strError))
-      end
-
-      -- Copy the file.
-      tResult, strError = self:copy(strSrcAbs, strDstPath)
-      if tResult~=true then
-        error(string.format('Failed to copy "%s" to "%s": %s', strSrcAbs, strDstPath, strError))
-      end
+  else
+    -- The second argument must be either a list of strings or a string.
+    local astrSrc = nil
+    if strSrcType=='string' then
+      astrSrc = { tSrc }
+    elseif strSrcType=='table' then
+      astrSrc = tSrc
     else
-      -- Copy a complete directory.
+      error(string.format('The "install" method was called with an invalid "tSrc" argument. It must be either a string or a table.'))
+    end
+    -- Loop over all elements and check their type.
+    local astrErrors = {}
+    for uiCnt, tValue in ipairs(astrSrc) do
+      local strType = type(tValue)
+      if strType~='string' then
+        table.insert(astrErrors, tostring(uiCnt))
+      end
+    end
+    if #astrErrors~=0 then
+      error(string.format('The "install" method was called with an invalid "tSrc" argument. The table contains non-string elements at the following indices: %s', table.concat(astrErrors, ', ')))
+    end
 
-      -- Reconsider the destination path. If the filename is set, add it to the directors.
-      if strDstFilename~=nil then
-        strDstDirname = self.pl.path.join(strDstDirname, strDstFilename)
-        strDstFilename = nil
+    -- The third argument must be a string.
+    if type(strDst)~='string' then
+      error(string.format('The "install" method was called with an invalid "strDst" argument. It must be a string.'))
+    end
+
+    -- Replace the ${} strings.
+    for uiCnt, strValue in ipairs(astrSrc) do
+      astrSrc[uiCnt] = self:replace_template(strValue)
+    end
+    strDst = self:replace_template(strDst)
+
+    -- The destination is treated as a directory...
+    --   if it ends with a slash or
+    --   if the source is a list with more than one element.
+    local fDstIsDir = nil
+    if string.sub(strDst, -1)=='/' then
+      fDstIsDir = true
+    elseif #astrSrc>1 then
+      fDstIsDir = true
+    else
+      fDstIsDir = false
+    end
+
+    -- Get the directory part of the destination.
+    local strDstDirname = nil
+    local strDstFilename = nil
+    if fDstIsDir==true then
+      strDstDirname = strDst
+    else
+      strDstDirname, strDstFilename = self.pl.path.splitpath(strDst)
+    end
+
+    -- Loop over all elements in the source list.
+    for _, strSrc in pairs(astrSrc) do
+      self.tLogInstallHelper.info('Installing "%s"...', strSrc)
+      -- Get the absolute path for the current source.
+      local strSrcAbs = self.pl.path.abspath(strSrc, self.strCwd)
+
+      -- Does the source exist?
+      if self.pl.path.exists(strSrcAbs)~=strSrcAbs then
+        error(string.format('The source path "%s" does not exist.', strSrcAbs))
       end
 
-      for strRoot, _, astrFiles in self.pl.dir.walk(strSrcAbs, false, true) do
-        -- Get the relative path from the depack folder to the current root.
-        local strRootRel = self.pl.path.relpath(strRoot, strSrcAbs)
+      -- Is the source a folder or a file.
+      local fIsDir = self.pl.path.isdir(strSrcAbs)
+      local fIsFile = self.pl.path.isfile(strSrcAbs)
+      if (fIsDir==true) and (fIsFile==true) then
+        error(string.format('"%s" is both a file and a directory.', strSrcAbs))
+      elseif (fIsDir==false) and (fIsFile==false) then
+        error(string.format('"%s" is neither a file nor a directory.', strSrcAbs))
+      end
 
-        -- Create the root folder. This is important for empty folders.
-        local strDstDir = self.pl.path.join(strDstDirname, strRootRel)
-        local tResult, strError = self.pl.dir.makepath(strDstDir)
-        if tResult~=true then
-          error(string.format('Failed to create the output folder "%s": %s', strDstDir, strError))
+      if fIsFile==true then
+        -- Copy one single file.
+
+        -- Get the filename of the source without the directory part.
+        local strSrcFilename = self.pl.path.basename(strSrcAbs)
+
+        -- Get the destination path.
+        local strDstPath = nil
+        if fDstIsDir==true then
+          strDstPath = self.pl.path.join(strDstDirname, strSrcFilename)
+        else
+          strDstPath = self.pl.path.join(strDstDirname, strDstFilename)
         end
 
-        -- Loop over all files and copy them.
-        for _, strFile in pairs(astrFiles) do
-          local strSrcPath = self.pl.path.join(strRoot, strFile)
-          local strDstPath = self.pl.path.join(strDstDir, strFile)
+        -- FIXME: check if the path is below the install base folder.
 
-          -- FIXME: check if the path is below the install base folder.
+        -- Was this file already installed?
+        local strPackage = self.atInstalledFiles[strDstPath]
+        if strPackage~=nil then
+          -- Yes -> refuse to overwrite it.
+          error(string.format('The file "%s" was already installed by the artifact %s.', strDstPath, strPackage))
+        end
+        self.atInstalledFiles[strDstPath] = self.strGMAV
 
-          -- Was this file already installed?
-          local strPackage = self.atInstalledFiles[strDstPath]
-          if strPackage~=nil then
-            -- Yes -> refuse to overwrite it.
-            error(string.format('The file "%s" was already installed by the artifact %s.', strDstPath, strPackage))
-          end
-          self.atInstalledFiles[strDstPath] = self.strGMAV
+        -- Create the output folder.
+        local tResult, strError = self.pl.dir.makepath(strDstDirname)
+        if tResult~=true then
+          error(string.format('Failed to create the output folder "%s": %s', strDstDirname, strError))
+        end
 
-          -- Copy the file.
-          local tResult, strError = self:copy(strSrcPath, strDstPath)
+        -- Copy the file.
+        tResult, strError = self:copy(strSrcAbs, strDstPath)
+        if tResult~=true then
+          error(string.format('Failed to copy "%s" to "%s": %s', strSrcAbs, strDstPath, strError))
+        end
+      else
+        -- Copy a complete directory.
+
+        -- Reconsider the destination path. If the filename is set, add it to the directors.
+        if strDstFilename~=nil then
+          strDstDirname = self.pl.path.join(strDstDirname, strDstFilename)
+          strDstFilename = nil
+        end
+
+        for strRoot, _, astrFiles in self.pl.dir.walk(strSrcAbs, false, true) do
+          -- Get the relative path from the depack folder to the current root.
+          local strRootRel = self.pl.path.relpath(strRoot, strSrcAbs)
+
+          -- Create the root folder. This is important for empty folders.
+          local strDstDir = self.pl.path.join(strDstDirname, strRootRel)
+          local tResult, strError = self.pl.dir.makepath(strDstDir)
           if tResult~=true then
-            error(string.format('Failed to copy "%s" to "%s": %s', strSrcPath, strDstPath, strError))
+            error(string.format('Failed to create the output folder "%s": %s', strDstDir, strError))
+          end
+
+          -- Loop over all files and copy them.
+          for _, strFile in pairs(astrFiles) do
+            local strSrcPath = self.pl.path.join(strRoot, strFile)
+            local strDstPath = self.pl.path.join(strDstDir, strFile)
+
+            -- FIXME: check if the path is below the install base folder.
+
+            -- Was this file already installed?
+            local strPackage = self.atInstalledFiles[strDstPath]
+            if strPackage~=nil then
+              -- Yes -> refuse to overwrite it.
+              error(string.format('The file "%s" was already installed by the artifact %s.', strDstPath, strPackage))
+            end
+            self.atInstalledFiles[strDstPath] = self.strGMAV
+
+            -- Copy the file.
+            local tResult, strError = self:copy(strSrcPath, strDstPath)
+            if tResult~=true then
+              error(string.format('Failed to copy "%s" to "%s": %s', strSrcPath, strDstPath, strError))
+            end
           end
         end
       end
